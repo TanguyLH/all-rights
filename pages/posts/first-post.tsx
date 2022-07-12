42671084137d17b1c8bf721269fba2849c6d3523
Tanguy Le Henaff <tanguy.le-henaff@kawantech.com>
2022-07-12 22:04:48 +0200

    polishing style

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Script from 'next/script';
+import Layout from '../../components/layout'
+import Head from 'next/head';
+
+export default function firstPost() {
+    return(
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>
+                    First Post
+                </h1>
+                <h2 className="title">
+                    Let's go{' '}
+                    <Link href="/">
+                        <a>back home</a>
+                    </Link>
+                </h2>
+            </Layout>
+        </>
+    );
+}
